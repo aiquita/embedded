9a557c0af632cf24db5aa27e8b6f72e417e45d32
ricffb <35438508+ricffb@users.noreply.github.com>
2021-01-24 12:49:43 +0100

    update genereted code

--- main_arduino/libpacemaker.h
+++ main_arduino/libpacemaker.h
@@ -4,7 +4,6 @@ extern "C"
 #endif
 void PACEMAKER_I_HEART_BEAT();
 void PACEMAKER_I_INT(int);
-void PACEMAKER_I_AMPLITUDE(int);
 void PACEMAKER_O_BPM(int);
 // void PACEMAKER_O_PASSED(int);
 void PACEMAKER_O_TIME_OUT();
