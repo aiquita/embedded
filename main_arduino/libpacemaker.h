cfc975bfa17838aecd3f82953e10075f187650e7
ricffb <35438508+ricffb@users.noreply.github.com>
2021-01-29 09:33:20 +0100

    make TIME_OUT observable

--- main_arduino/libpacemaker.h
+++ main_arduino/libpacemaker.h
@@ -5,7 +5,6 @@ extern "C"
 void PACEMAKER_I_HEART_BEAT();
 void PACEMAKER_I_INT(int);
 void PACEMAKER_O_BPM(int);
-// void PACEMAKER_O_PASSED(int);
 void PACEMAKER_O_TIME_OUT();
 int PACEMAKER();
 #if defined(__cplusplus)
